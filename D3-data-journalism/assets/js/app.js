a6183d5086906ebf9f136d5cb2a8cfd2b190cff7
SethAbbott916 <sethcode91@gmail.com>
2020-08-03 16:06:42 -0700

    Rough Draft

--- D3-data-journalism/assets/js/app.js
+++ D3-data-journalism/assets/js/app.js
@@ -12,7 +12,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select(".scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -20,4 +20,49 @@ var svg = d3.select(".chart")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  
+// Import Data
+d3.csv("data.csv").then(function(censusdata) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    censusdata.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.age = +data.age;
+        data.income = +data.income;
+        data.healthcare = +data.healthcare;
+        data.obesity = +data.obesity;
+        data.smokes = +data.smokes;
+        console.log(data.poverty)
+    });
+    console.log(data.poverty)
+
+    var xLinearScale = d3.scaleLinear()
+    .domain([20, d3.max(censusdata, d => d.poverty)])
+    .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(censusdata, d => d.obesity)])
+    .range([height, 0]);
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+    chartGroup.append("g")
+    .call(leftAxis);
+
+    var circlesGroup = chartGroup.selectAll("g")
+    .data(census)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.obesity))
+    .attr("r", "15")
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
+
+
+});
